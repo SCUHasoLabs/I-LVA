b8ab1bc1a6b49541f6a079a2fe875fefc335515f
Jason Wu <74744133+JDubWeuu@users.noreply.github.com>
2024-01-02 17:07:39 -0800

    Update ml_to_arduino.py

--- Demo_ML_to_Arduino/ml_to_arduino.py
+++ Demo_ML_to_Arduino/ml_to_arduino.py
@@ -26,7 +26,7 @@ ser = serial.Serial("COM3", 9600)
 time.sleep(2)
 
 # hand movement was detected
-classification_result = '1' if np.argmax(model.predict(X_test_scaled_df), axis=-1) in [0, 2, 3, 4, 5, 6, 7] else '0'
+classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[0] in [0, 2, 3, 4, 5, 6, 7] else "0"
 
 # will write 0 or 1 to the serial port of the arduino
 ser.write(classification_result.encode())

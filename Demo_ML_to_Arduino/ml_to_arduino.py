0f1f2dca9185774451e827ae2e8417cfb1c0b5ca
Jason Wu <wu80jason8@gmail.com>
2024-01-10 22:15:24 -0800

    Finished implementing extracting data from csv to send to arduino

--- Demo_ML_to_Arduino/ml_to_arduino.py
+++ Demo_ML_to_Arduino/ml_to_arduino.py
@@ -5,13 +5,15 @@ import pandas as pd
 import joblib
 import numpy as np
 
-model = keras.models.load_model("my_model.h5")
+model = keras.models.load_model("Demo_ML_to_Arduino/my_model.h5")
 
 channels = {}
 
 
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("Demo_ML_to_Arduino/data.csv")
+df = df.sample(frac=1).reset_index(drop=True)
+df2 = df.copy()
 df.drop("time", axis=1, inplace=True)
 df.drop("class", axis=1, inplace=True)
 
@@ -24,18 +26,17 @@ for i in range(50):
         channels['channel' + str(j)].append(channel)
 
 df = pd.DataFrame(channels)
-print(df)
 
-scaler = joblib.load("scaler.save")
+scaler = joblib.load("Demo_ML_to_Arduino/scaler.save")
 X_test_scaled = scaler.transform(df)
 X_test_scaled_df = pd.DataFrame(X_test_scaled, columns=df.columns)
 
 # make sure the serial port and baud rates are consistent with the arduino
-ser = serial.Serial("COM6", 9600)
+ser = serial.Serial("COM3", 9600)
 time.sleep(2)
 
 # hand movement was detected
-classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[0] in [0, 2, 3, 4, 5, 6, 7] else "0"
+classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[10] in [0, 2, 3, 4, 5, 6, 7] else "0"
 
 # will write 0 or 1 to the serial port of the arduino
 ser.write(classification_result.encode())

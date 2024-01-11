f76e10e85385b145c9e02a5e0fa3b81f101d78e4
Vikram <vjangid@scu.edu>
2024-01-10 18:28:35 -0800

    updated the script to direclty read from the data.csv file

--- Demo_ML_to_Arduino/ml_to_arduino.py
+++ Demo_ML_to_Arduino/ml_to_arduino.py
@@ -9,26 +9,35 @@ model = keras.models.load_model("my_model.h5")
 
 channels = {}
 
-for i in range(8):
-    print(f"channel{i+1}")
-    channel = float(input())
-    channels["channel" + str(i + 1)] = channel
 
-df = pd.DataFrame([channels])
-print(df.head())
+
+df = pd.read_csv("data.csv")
+df.drop("time", axis=1, inplace=True)
+df.drop("class", axis=1, inplace=True)
+
+
+for i in range(50):
+    for j in range(1,9):
+        channel = df.iloc[i][f'channel{j}']
+        if not channels.get(f'channel{j}'):
+            channels['channel' + str(j)] = []
+        channels['channel' + str(j)].append(channel)
+
+df = pd.DataFrame(channels)
+print(df)
 
 scaler = joblib.load("scaler.save")
 X_test_scaled = scaler.transform(df)
 X_test_scaled_df = pd.DataFrame(X_test_scaled, columns=df.columns)
 
-# make sure the serial port and baud rates are consistent with the arduino
-ser = serial.Serial("COM3", 9600)
-time.sleep(2)
+# # make sure the serial port and baud rates are consistent with the arduino
+# ser = serial.Serial("COM6", 9600)
+# time.sleep(2)
 
-# hand movement was detected
-classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[0] in [0, 2, 3, 4, 5, 6, 7] else "0"
+# # hand movement was detected
+# classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[0] in [0, 2, 3, 4, 5, 6, 7] else "0"
 
-# will write 0 or 1 to the serial port of the arduino
-ser.write(classification_result.encode())
+# # will write 0 or 1 to the serial port of the arduino
+# ser.write(classification_result.encode())
 
-ser.close()
+# ser.close()

c3bf3186eb93c3db3d73ec8b10e35ea02f353af3
Vikram <vjangid@scu.edu>
2024-01-12 15:42:22 -0800

    irrelevant push

--- Demo_ML_to_Arduino/ml_to_arduino.py
+++ Demo_ML_to_Arduino/ml_to_arduino.py
@@ -10,8 +10,9 @@ model = keras.models.load_model("my_model.h5")
 channels = {}
 
 
-
 df = pd.read_csv("data.csv")
+df = df.sample(frac=1).reset_index(drop=True)
+df2 = df.copy()
 df.drop("time", axis=1, inplace=True)
 df.drop("class", axis=1, inplace=True)
 
@@ -24,7 +25,6 @@ for i in range(50):
         channels['channel' + str(j)].append(channel)
 
 df = pd.DataFrame(channels)
-print(df)
 
 scaler = joblib.load("scaler.save")
 X_test_scaled = scaler.transform(df)
@@ -35,7 +35,7 @@ ser = serial.Serial("COM6", 9600)
 time.sleep(2)
 
 # hand movement was detected
-classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[0] in [0, 2, 3, 4, 5, 6, 7] else "0"
+classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[10] in [0, 2, 3, 4, 5, 6, 7] else "0"
 
 # will write 0 or 1 to the serial port of the arduino
 ser.write(classification_result.encode())

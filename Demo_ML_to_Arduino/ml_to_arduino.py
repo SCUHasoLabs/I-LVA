3f4fea441d4510e1c05df8d6454dbf537be51a85
Jason Wu <wu80jason8@gmail.com>
2024-01-12 15:50:42 -0800

    python to arduino

--- Demo_ML_to_Arduino/ml_to_arduino.py
+++ Demo_ML_to_Arduino/ml_to_arduino.py
@@ -10,7 +10,10 @@ model = keras.models.load_model("Demo_ML_to_Arduino/my_model.h5")
 channels = {}
 
 
-df = pd.read_csv("data.csv")
+
+df = pd.read_csv("Demo_ML_to_Arduino/data.csv")
+df = df.sample(frac=1).reset_index(drop=True)
+df2 = df.copy()
 df.drop("time", axis=1, inplace=True)
 df.drop("class", axis=1, inplace=True)
 
@@ -33,7 +36,11 @@ ser = serial.Serial("COM3", 9600)
 time.sleep(2)
 
 # hand movement was detected
-classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[0] in [0, 2, 3, 4, 5, 6, 7] else "0"
+for i in range(len(50)):
+    classification_result = "1" if np.argmax(model.predict(X_test_scaled_df), axis=-1)[i] in [0, 2, 3, 4, 5, 6, 7] else "0"
+    ser.write(classification_result.encode())
+    # send classification result every 3 seconds
+    time.sleep(3)
 
 # will write 0 or 1 to the serial port of the arduino
 ser.close()
